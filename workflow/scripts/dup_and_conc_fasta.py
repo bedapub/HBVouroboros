d560978445544b40498f2df2b043804b5a97dd42
Jitao David Zhang <jitao_david.zhang@roche.com>
2020-03-11 12:41:46 +0100

    assignment -> testing equal

---
+++ workflow/scripts/dup_and_conc_fasta.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+def dup_and_conc_fasta(infile="hbvdbr.fas", outfile="hbvdbr-dupconc.bas"):
+  """Duplicate sequences in a FASTA file, concatenate the original 
+     with the duplicates, and write the concatenated sequences
+
+  Args:
+      infile (str): The input filename, pointing to a FASTA file
+      outfile (str): The output filename, overwritten if the file exists.
+
+  Returns:
+      int: number of sequences 
+  """
+
+  count = 0
+  with open(outfile, 'w') as fout:
+    with open(infile, 'r') as fin:
+      while True:
+        line = fin.readline().strip()
+        
+        if not line:
+            break;
+      if line[0]==">":
+        count += 1
+        fout.write(line)
+      else:
+        conc = line + line
+        fout.write(conc)
+
+  return(count)

0b033bec1cd343585aab6c9cf9af1f05c4e3e758
Jitao David Zhang <jitao_david.zhang@roche.com>
2020-03-11 14:54:28 +0100

    do not download the file unless it is not there

--- workflow/scripts/main.py
+++ workflow/scripts/main.py
@@ -2,7 +2,9 @@
 
 import download_HBV_refgenomes
 import dup_and_conc_fasta
+import os.path
 
-download_HBV_refgenomes.download_HBV_refgenomes("test.fasta")
+if not os.path.exists("test.fasta"):
+  download_HBV_refgenomes.download_HBV_refgenomes("test.fasta")
 dup_and_conc_fasta.dup_and_conc_fasta("test.fasta", "test-dupconc.fasta")
 

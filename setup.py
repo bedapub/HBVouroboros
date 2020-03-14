538a0e7d2d7ea8f6710e69083bacbef848aeb064
Jitao David Zhang <jitao_david.zhang@roche.com>
2020-03-14 09:56:37 +0100

    version bumping

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'HBVouroboros',
-    version = '0.2',
+    version = '0.3',
     description = "Map reads to HBV cccDNA genomes",
     packages=find_packages(),
 

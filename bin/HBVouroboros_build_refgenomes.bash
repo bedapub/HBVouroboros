2de341858cb0cc88d67b53d04595c4f95bd79106
Jitao David Zhang <jitao_david.zhang@roche.com>
2020-03-16 14:37:26 +0100

    move bash to py scripts, and rename the subdirectory build_HBV_refgenomes to build_refgenomes

--- bin/HBVouroboros_build_refgenomes.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# the script builds indices of HBV reference genomes
-
-## the following line is needed to activate conda within a bash script
-## see https://github.com/conda/conda/issues/7980
-eval "$(conda shell.bash hook)"
-conda activate HBVouroboros
-snakemake --use-conda -d ../HBV_refgenomes/ $@

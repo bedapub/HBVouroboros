04b759a3fc7694f8ea3b01d57ab62c0e25463819
Jitao David Zhang <jitao_david.zhang@roche.com>
2020-07-13 14:44:48 +0200

    files updated: note that the package files are not checked for existence

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='HBVouroboros',
-    version='0.6.1',
+    version='0.6.2',
     description="sequence-based HBV genotyping and expression profiling",
     packages=find_packages(),
 
@@ -17,13 +17,17 @@ setup(
     scripts = [
         'bin/HBVouroboros_map_samples.py',
         'bin/HBVouroboros_map_biokit.py',
-        'bin/HBVouroboros_build_refgenomes.py'
+        'bin/HBVouroboros_build_refgenomes.py',
+        'bin/HBVouroboros_trimmomatic.py'
         ],
     package_data = {
         "HBVouroboros": ["build_refgenomes/Snakefile", 
             "align_reads/Snakefile",
             "align_reads/config/cluster.json",
             "align_reads/config/config.yaml",
+            "trim_reads/Snakefile",
+            "trim_reads/config/primer.txt",
+            "trim_reads/config/config.yaml",
             "biokit/Snakefile"]
     }
     # install_requires = ['requests>=2.19.1']

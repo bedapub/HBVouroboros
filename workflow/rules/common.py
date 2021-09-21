12fb723d9691d742b5540e7f6571c880b110cdae
Milad4849 <70942846+Milad4849@users.noreply.github.com>
2021-09-21 15:04:43 +0200

    Update common.py

--- workflow/rules/common.py
+++ workflow/rules/common.py
@@ -88,7 +88,7 @@ def collect_gene_coverage(coverage_files, outfile, feat_type='gene'):
     """Collect gene coverage files into a GCT outfile
 
     Args:
-        coverage_files (str): a list of coverage files exported by
+        coverage_files (list(str)): a list of coverage files exported by
             'bedtools coverage'. They must be generated from the same
             GFF3 files.
         outfile (str): Output file name
@@ -201,7 +201,6 @@ def dedup_file(infile, outfile):
     Returns:
         value of pandas.DataFrame.to_csv
     """
-
     depth = read_table(infile)
     dedup_res = dedup(depth)
     res = dedup_res.to_csv(outfile, sep='\t', index=False)
@@ -406,7 +405,7 @@ def dup_and_conc_FASTA(infile, outfile):
     Returns:
         int: number of sequences
     """
-
+    print(str(infile))
     sequences = SeqIO.parse(infile, 'fasta')
     outseqs = (dup_and_conc(record) for record in sequences)
     res = SeqIO.write(outseqs, outfile, 'fasta')
@@ -434,3 +433,59 @@ def split_FASTA(infile, outdir=None, prefix=''):
                        prefix + seq.id.replace('|', '_') + '.fasta')
         SeqIO.write(seq, outfile, 'fasta')
         count += 1
+
+def vcfClean(vcfFile, outfile):
+	vcfile = open(vcfFile, "r")
+	vcfLines = vcfile.readlines()
+
+	gnLengths = dict()
+	for line in vcfLines:
+		if "##contig=<ID" in line:
+			x = line.split(',')
+			for term in x:
+				if ("length" in term)==True:
+					gnLengths[line[24:32].split("_")[0]] = term.split('=')[1].split('>')[0]
+
+	varPos =list()	
+	with open(outfile, 'w') as filehandle:
+		print (outfile)
+		for i in range(len(vcfLines)):
+			aline = vcfLines[i]
+			if (aline[0:4] == 'gnl|'):
+				if (int(gnLengths[aline.split("|")[2].split("_")[0]])/2 >= int(aline.split()[1])):
+					filehandle.write('%s' % aline)
+				else:
+					updatedPos = int(aline.split()[1]) - int(gnLengths[aline.split("|")[2].split("_")[0]])/2
+					aline = aline.replace(aline.split()[1],str(updatedPos))
+					if str(aline.split()[1]) not in varPos:
+						filehandle.write('%s' % aline)
+						varPos.append(str(aline.split()[1]))
+			else: 
+				(filehandle.write('%s' % aline))
+	return(True)
+
+
+def test_cleanvcf(vcfFile):
+	vcfile = open(vcfFile, "r")
+	vcfLines = vcfile.readlines()
+
+	gnLengths = dict()
+	for line in vcfLines:
+		if "##contig=<ID" in line:
+			x = line.split(',')
+			for term in x:
+				if ("length" in term)==True:
+					gnLengths[line[24:32].split("_")[0]] = term.split('=')[1].split('>')[0]
+
+	varPos =list()	
+	for i in range(len(vcfLines)):
+		aline = vcfLines[i]
+		if (aline[0:4] == 'gnl|'):
+			if (int(gnLengths[aline.split("|")[2].split("_")[0]])/2 >= int(aline.split()[1])):
+				varPos.append(str(aline.split()[1]))
+			else:
+				updatedPos = int(aline.split()[1]) - int(gnLengths[aline.split("|")[2].split("_")[0]])/2
+				if str(aline.split()[1]) not in varPos:
+					varPos.append(str(aline.split()[1]))
+
+	return(varPos)

b166732c49e94c6c384a2b6699c28fe2fbd8e983
Milad4849 <70942846+Milad4849@users.noreply.github.com>
2021-12-30 01:02:29 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,6 +26,8 @@ sys.path.insert(0, os.path.abspath('../..'))
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
 sys.path.insert(0, os.path.abspath(os.path.join('..', '..', 'workflow/rules')))
+sys.path.insert(0, os.path.abspath('./../..'))
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
